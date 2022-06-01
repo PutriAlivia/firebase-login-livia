31204b9a42ba5e3e4352a81dbb77057cb4489892
Putri Alivia <putrialivia666@gmail.com>
2022-06-01 17:42:18 +0700

    edit file index dan app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Root from "./Root";
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
