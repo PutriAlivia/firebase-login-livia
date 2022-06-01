f355339a8929adf732b0d17d8f68cbd1b2f85917
Putri Alivia <putrialivia666@gmail.com>
2022-06-01 17:46:43 +0700

    membuat component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Root from "./Root";
+import Root from "./root";
 
 import reportWebVitals from './reportWebVitals';
 
